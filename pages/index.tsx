655a69bc5286f02c95e4ae04761ad607a47c749d
jedinrk <jedi.nrk@gmail.com>
2023-04-02 13:35:08 +0400

    updated Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,14 @@ import Vaults from "./vaults";
 
 function Home() {
 
-  
-
   return (
     <div>
       <Head>
-        <title>next-web3-boilerplate</title>
+        <title>YeeldBox</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Vaults></Vaults>
-  
+
     </div>
   );
 }
