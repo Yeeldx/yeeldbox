3aa3fe942a61c70908f7c01c2a981804bc41bdfd
jedinrk <jedi.nrk@gmail.com>
2023-04-03 14:48:35 +0400

    setting up production deployment script

--- next.config.js
+++ next.config.js
@@ -4,5 +4,18 @@
  * @type {import('next').NextConfig}
  **/
 module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/yeeldbox": { page: "/yeeldbox" },
+      "/docs": { page: "/docs" },
+    };
+  },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
